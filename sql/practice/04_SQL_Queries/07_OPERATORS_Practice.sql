90eb45856b793e58cd93721dfff948224d454117
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 09:50:56 +0530

    Create 07_OPERATORS_Practice.sql

---
+++ sql/practice/04_SQL_Queries/07_OPERATORS_Practice.sql
@@ -0,0 +1,39 @@
+/*
+=========================================
+OPERATORS Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Show employees with salary > 50000.
+
+-- 2. Calculate annual salary.
+
+-- 3. Show products with stock < 100.
+
+------------------------------------------------------------
+
+-- Intermediate
+
+-- 4. Show employees from departments 2 or 4.
+
+-- 5. Display customer full names.
+
+-- 6. Find employees with even employee IDs.
+
+------------------------------------------------------------
+
+-- Advanced
+
+-- 7. Show employees earning between 50000 and 80000
+--    using only comparison operators.
+
+-- 8. Show products where
+--    units_in_stock * unit_price > 500000.
+
+-- 9. Show employees not working
+--    in department 1.
+
+-- 10. Combine AND, OR and NOT
+--     in a single query.
