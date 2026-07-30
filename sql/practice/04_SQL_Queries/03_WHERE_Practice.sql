9b914f461e2cbeb3afcb881db61257e287deee5e
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 09:47:39 +0530

    Create 03_WHERE_Practice.sql

---
+++ sql/practice/04_SQL_Queries/03_WHERE_Practice.sql
@@ -0,0 +1,35 @@
+/*
+=========================================
+WHERE Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Show employees with salary greater than 50,000.
+
+-- 2. Show customers from Delhi.
+
+-- 3. Show products with unit_price greater than 50,000.
+
+------------------------------------------------------------
+
+-- Intermediate
+
+-- 4. Show employees hired after '2022-01-01'.
+
+-- 5. Show customers from Mumbai or Pune.
+
+-- 6. Show products with units_in_stock less than 100.
+
+------------------------------------------------------------
+
+-- Advanced
+
+-- 7. Show IT employees earning more than 60,000.
+
+-- 8. Show products priced below 20,000 with stock greater than 50.
+
+-- 9. Show customers who are not from Delhi.
+
+-- 10. Show employees from departments 2 or 4 with salary above 50,000.
