c294d08186d4ebf05b74ca0c7742fdcad25eecc4
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 09:52:59 +0530

    Create 10_LIKE_Practice.sql

---
+++ sql/practice/04_SQL_Queries/10_LIKE_Practice.sql
@@ -0,0 +1,36 @@
+/*
+=========================================
+LIKE Operator Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Show employees whose names start with 'R'.
+
+-- 2. Show customers whose city ends with 'pur'.
+
+-- 3. Show Gmail users.
+
+------------------------------------------------------------
+
+-- Intermediate
+
+-- 4. Show products containing 'Phone'.
+
+-- 5. Show employees whose names have exactly five letters.
+
+-- 6. Show cities beginning with 'M'.
+
+------------------------------------------------------------
+
+-- Advanced
+
+-- 7. Show employees whose names contain 'an'.
+
+-- 8. Show customers using Yahoo email.
+
+-- 9. Show products beginning with 'Smart'.
+
+-- 10. Challenge:
+-- Find all customers whose email domain is '.org'.
