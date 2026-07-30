6bab283885884e4d427a916c3c041a17359f2691
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 09:45:13 +0530

    Create 01_INSERT_Practice.sql

---
+++ sql/practice/04_SQL_Queries/01_INSERT_Practice.sql
@@ -0,0 +1,26 @@
+-- Beginner
+
+-- Insert one department
+
+-- Insert one employee
+
+-- Insert one customer
+
+------------------------------------------------------------
+
+-- Intermediate
+
+-- Insert multiple products
+
+-- Insert multiple suppliers
+
+------------------------------------------------------------
+
+-- Advanced
+
+-- Insert records using RETURNING
+
+-- Verify inserted rows
+
+-- Challenge:
+-- Insert a customer and then create an order for that customer.
