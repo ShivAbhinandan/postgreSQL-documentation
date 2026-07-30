4e9e5be5e2ca4cbf040f4bf69746a97e8ebad7fb
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 09:49:27 +0530

    Create 05_LIMIT_OFFSET_Practice.sql

---
+++ sql/practice/04_SQL_Queries/05_LIMIT_OFFSET_Practice.sql
@@ -0,0 +1,35 @@
+/*
+=========================================
+LIMIT & OFFSET Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Show the first 3 employees.
+
+-- 2. Show the first 5 products.
+
+-- 3. Show the first 2 customers.
+
+------------------------------------------------------------
+
+-- Intermediate
+
+-- 4. Skip the first 2 employees.
+
+-- 5. Show 4 products after skipping 1.
+
+-- 6. Show the 5 newest orders.
+
+------------------------------------------------------------
+
+-- Advanced
+
+-- 7. Show the top 3 highest-paid employees.
+
+-- 8. Show the 5 cheapest products.
+
+-- 9. Show page 3 of customers (2 customers per page).
+
+-- 10. Show the newest 2 invoices.
