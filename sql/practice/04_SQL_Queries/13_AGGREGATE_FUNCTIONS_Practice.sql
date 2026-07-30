5798e77b6b46265ecf103f3a0daf165d05e74569
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 10:11:50 +0530

    Create 13_AGGREGATE_FUNCTIONS_Practice.sql

---
+++ sql/practice/04_SQL_Queries/13_AGGREGATE_FUNCTIONS_Practice.sql
@@ -0,0 +1,37 @@
+/*
+=========================================
+AGGREGATE FUNCTIONS Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Count all employees.
+
+-- 2. Find the average salary.
+
+-- 3. Find the maximum salary.
+
+------------------------------------------------------------
+
+-- Intermediate
+
+-- 4. Count employees with email addresses.
+
+-- 5. Find total sales.
+
+-- 6. Find minimum stock quantity.
+
+------------------------------------------------------------
+
+-- Advanced
+
+-- 7. Count distinct departments.
+
+-- 8. Find average salary for department 3.
+
+-- 9. Display total revenue
+--    for completed orders.
+
+-- 10. Display the count of products
+--     with a non-NULL supplier_id.
