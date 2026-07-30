0c7cb13c96581e2239d1f9c6ef3b876580e04436
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 09:54:13 +0530

    Create 11_IS_NULL_Practice.sql

---
+++ sql/practice/04_SQL_Queries/11_IS_NULL_Practice.sql
@@ -0,0 +1,40 @@
+/*
+=========================================
+IS NULL Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Show employees without phone numbers.
+
+-- 2. Show customers with email addresses.
+
+-- 3. Show products without supplier IDs.
+
+------------------------------------------------------------
+
+-- Intermediate
+
+-- 4. Show employees in department 2
+--    without phone numbers.
+
+-- 5. Show customers with phone numbers
+--    ordered alphabetically.
+
+-- 6. Show products that have suppliers.
+
+------------------------------------------------------------
+
+-- Advanced
+
+-- 7. Show employees whose bonus is NULL.
+
+-- 8. Show customers without email addresses.
+
+-- 9. Show products with NULL supplier IDs
+--    ordered by product_name.
+
+-- 10. Challenge:
+-- Replace NULL phone numbers with
+-- 'Not Available' using COALESCE().
