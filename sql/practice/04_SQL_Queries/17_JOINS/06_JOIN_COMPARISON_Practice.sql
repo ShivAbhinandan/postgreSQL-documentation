d8972b2ef64088ba91743679cc0b4a53dc9511a6
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 11:03:27 +0530

    Create 06_JOIN_COMPARISON_Practice.sql

---
+++ sql/practice/04_SQL_Queries/17_JOINS/06_JOIN_COMPARISON_Practice.sql
@@ -0,0 +1,33 @@
+/*
+=========================================
+JOIN COMPARISON Practice
+=========================================
+*/
+
+-- 1. Which join returns only matching rows?
+
+-- 2. Which join returns every row
+-- from the left table?
+
+-- 3. Which join returns every row
+-- from the right table?
+
+-- 4. Which join returns every row
+-- from both tables?
+
+-------------------------------------------------------
+
+-- Scenario Based
+
+-- 5. Customers without orders.
+
+-- 6. Products never sold.
+
+-- 7. Departments without employees.
+
+-- 8. Compare two customer databases.
+
+-- 9. Compare inventory systems.
+
+-- 10. Explain why LEFT JOIN
+-- is generally preferred over RIGHT JOIN.
