f51880d464eb0f593e2ffba0a82e9aea39365a9e
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 10:10:09 +0530

    Create 12_ALIAS_Practice.sql

---
+++ sql/practice/04_SQL_Queries/12_ALIAS_Practice.sql
@@ -0,0 +1,35 @@
+/*
+=========================================
+ALIAS Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Rename first_name as employee_name.
+
+-- 2. Rename salary as monthly_salary.
+
+-- 3. Display annual salary using an alias.
+
+------------------------------------------------------------
+
+-- Intermediate
+
+-- 4. Use aliases for employees and departments.
+
+-- 5. Rename unit_price as selling_price.
+
+-- 6. Display total employees using COUNT() and an alias.
+
+------------------------------------------------------------
+
+-- Advanced
+
+-- 7. Display employee full names using aliases.
+
+-- 8. Use aliases in a subquery.
+
+-- 9. Write a JOIN using table aliases.
+
+-- 10. Use an alias in ORDER BY.
