0b1a8fc3bba362d0f53559feecdbc92541f15a29
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 09:46:35 +0530

    Create 02_SELECT_Practice.sql

---
+++ sql/practice/04_SQL_Queries/02_SELECT_Practice.sql
@@ -0,0 +1,36 @@
+/*
+=========================================
+SELECT Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Display all employees.
+
+-- 2. Display all products.
+
+-- 3. Display all customers.
+
+-------------------------------------------------
+
+-- Intermediate
+
+-- 4. Display first_name and last_name.
+
+-- 5. Display product_name and unit_price.
+
+-- 6. Display department_name and location.
+
+-------------------------------------------------
+
+-- Advanced
+
+-- 7. Display employee full name.
+
+-- 8. Display salary and salary after a 15% increase.
+
+-- 9. Display today's date using CURRENT_DATE.
+
+-- 10. Display a constant value:
+-- "Welcome to PostgreSQL Mastery"
