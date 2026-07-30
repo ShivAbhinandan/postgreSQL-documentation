2472a574401ad0b1263711f59684d787d0df78a8
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 12:32:39 +0530

    Create 11_Multiple_Joins_Practice.sql

---
+++ sql/practice/04_SQL_Queries/17_JOINS/11_Multiple_Joins_Practice.sql
@@ -0,0 +1,43 @@
+/*
+=========================================
+Multiple Joins Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Join Employees → Departments → Locations.
+
+-- 2. Display employee, department, and city.
+
+-- 3. Practice using aliases.
+
+---------------------------------------------------------
+
+-- Intermediate
+
+-- 4. Join Customers → Orders → Order Items → Products.
+
+-- 5. Add Categories.
+
+-- 6. Include customers without orders.
+
+---------------------------------------------------------
+
+-- Advanced
+
+-- 7. Join five tables.
+
+-- 8. Mix INNER JOIN and LEFT JOIN.
+
+-- 9. Analyze the query with EXPLAIN ANALYZE.
+
+-- 10. Challenge:
+-- Build a complete sales report containing:
+-- Customer
+-- Order Date
+-- Product
+-- Category
+-- Quantity
+-- Unit Price
+-- Total Amount
