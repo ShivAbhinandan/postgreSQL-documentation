8bcab9227b221b65df4b33dfc7478f1a05aa3e11
Shiv Abhinandan <167851975+ShivAbhinandan@users.noreply.github.com>
2026-07-30 09:52:22 +0530

    Create 09_IN_Practice.sql

---
+++ sql/practice/04_SQL_Queries/09_IN_Practice.sql
@@ -0,0 +1,41 @@
+/*
+=========================================
+IN Operator Practice
+=========================================
+*/
+
+-- Beginner
+
+-- 1. Show customers from Delhi and Pune.
+
+-- 2. Show employees from departments 1 and 4.
+
+-- 3. Show products with IDs 2, 4, and 6.
+
+------------------------------------------------------------
+
+-- Intermediate
+
+-- 4. Show customers NOT from Delhi or Mumbai.
+
+-- 5. Show employees from departments 2 and 4
+--    with salary greater than 60000.
+
+-- 6. Show orders with IDs 1, 3, and 4.
+
+------------------------------------------------------------
+
+-- Advanced
+
+-- 7. Show employees working in departments
+--    located in Hyderabad using a subquery.
+
+-- 8. Show products supplied by supplier IDs
+--    1 and 3.
+
+-- 9. Show customers whose city is NOT IN
+--    ('Delhi', 'Mumbai', 'Pune').
+
+-- 10. Challenge:
+-- Display products belonging to categories
+-- returned by a subquery.
